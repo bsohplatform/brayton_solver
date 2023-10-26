a5e5091589af7dafd900989fd0f5f25eaf0f35dc
bsohplatform <bongseongoh@kier.re.kr>
2023-10-26 20:01:47 +0900

    commit. 브레이튼 사이클 열교환기 엔탈피 기반으로 수정 (이상유동 열원 가능)

--- Brayton_solver.py
+++ Brayton_solver.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 import CoolProp.CoolProp as CP
 from scipy.optimize import fsolve
-
+import numpy as np
 
 @dataclass
 class Fluid_stream:
@@ -29,6 +29,8 @@ class Settings:
     expand_eff:float = 0.0
     mech_eff:float = 0.95
     gen_eff: float = 0.99
+    
+    N_element:int = 30
     
 @dataclass
 class Outputs:
@@ -177,36 +179,144 @@ class Brayton():
                 first['cold_in'].p = first['cold_out'].p / (1-settings.cold_dp)
                 first['expand_in'].p = first['hot_out'].p*(1-settings.ihx_hot_dp)
                 
-                first['hot_out'].T = second['hot_in'].T + settings.hot_T_pp
-                first['hot_out'].h = CP.PropsSI('H','T',first['hot_out'].T,'P',first['hot_out'].p,first['hot_out'].fluid)
-                first['cold_out'].T = second['cold_in'].T - settings.cold_T_pp
-                first['cold_out'].h = CP.PropsSI('H','T',first['cold_out'].T,'P',first['cold_out'].p,first['cold_out'].fluid)
+                b = 1
+                T_hout_lb = second['hot_in'].T
+                T_hout_ub = min(second['hot_in'].T + 300.0, 1000)
                 
-                ideal_dh = min(first['hot_out'].h - CP.PropsSI('H','T',first['cold_out'].T,'P',first['expand_in'].p,first['hot_out'].fluid),CP.PropsSI('H','T',first['hot_out'].T,'P',first['comp_in'].p,first['cold_out'].fluid)-first['cold_out'].h)
-                first['comp_in'].h = first['cold_out'].h + settings.ihx_eff*ideal_dh
-                first['comp_in'].T = CP.PropsSI('T','H',first['comp_in'].h,'P',first['comp_in'].p,first['comp_in'].fluid)
-                first['comp_in'].s = CP.PropsSI('S','H',first['comp_in'].h,'P',first['comp_in'].p,first['comp_in'].fluid)
-                comp_out_h_ideal = CP.PropsSI('H','P',first['hot_in'].p,'S',first['comp_in'].s,first['hot_in'].fluid)
-                outputs.comp_W = (comp_out_h_ideal - first['comp_in'].h)/settings.comp_eff
+                T_cout_lb = max(second['cold_in'].T - 300.0, 200.0)
+                T_cout_ub = second['cold_in'].T 
                 
-                first['hot_in'].h = first['comp_in'].h + outputs.comp_W
-                first['hot_in'].T = CP.PropsSI('T','H',first['hot_in'].h, 'P',first['hot_in'].p, first['hot_in'].fluid)
-                
-                first['expand_in'].h = first['hot_out'].h - settings.ihx_eff*ideal_dh
-                first['expand_in'].T = CP.PropsSI('T','H',first['expand_in'].h,'P',first['expand_in'].p,first['expand_in'].fluid)
-                first['expand_in'].s = CP.PropsSI('S','H',first['expand_in'].h,'P',first['expand_in'].p,first['expand_in'].fluid)
-                expand_out_h_ideal = CP.PropsSI('H','P',first['cold_in'].p,'S',first['expand_in'].s,first['cold_in'].fluid)
-                outputs.expand_W = (first['expand_in'].h - expand_out_h_ideal)*settings.expand_eff
-                
-                first['cold_in'].h = first['expand_in'].h - outputs.expand_W
-                first['cold_in'].T = CP.PropsSI('T','H',first['cold_in'].h, 'P',first['cold_in'].p, first['cold_in'].fluid)
-                
-                if process_case <= 2:
-                    first['cold_in'].m = second['cold_in'].q / (first['cold_out'].h - first['cold_in'].h)
-                    first['cold_out'].m = first['cold_in'].m
-                else:
-                    first['hot_in'].m = second['hot_in'].q / (first['hot_in'].h - first['hot_out'].h)
-                    first['hot_out'].m = first['hot_in'].m
+                while b:
+                    c = 1
+                    while c:
+                        T_hout = 0.5*(T_hout_lb+T_hout_ub)
+                        first['hot_out'].T = T_hout
+                        first['hot_out'].h = CP.PropsSI('H','T',first['hot_out'].T,'P',first['hot_out'].p,first['hot_out'].fluid)
+                        T_cout = 0.5*(T_cout_lb+T_cout_ub)
+                        first['cold_out'].T = T_cout
+                        first['cold_out'].h = CP.PropsSI('H','T',first['cold_out'].T,'P',first['cold_out'].p,first['cold_out'].fluid)
+                    
+                        ideal_dh = min(first['hot_out'].h - CP.PropsSI('H','T',first['cold_out'].T,'P',first['expand_in'].p,first['hot_out'].fluid),CP.PropsSI('H','T',first['hot_out'].T,'P',first['comp_in'].p,first['cold_out'].fluid)-first['cold_out'].h)
+                        first['comp_in'].h = first['cold_out'].h + settings.ihx_eff*ideal_dh
+                        first['comp_in'].T = CP.PropsSI('T','H',first['comp_in'].h,'P',first['comp_in'].p,first['comp_in'].fluid)
+                        first['comp_in'].s = CP.PropsSI('S','H',first['comp_in'].h,'P',first['comp_in'].p,first['comp_in'].fluid)
+                        comp_out_h_ideal = CP.PropsSI('H','P',first['hot_in'].p,'S',first['comp_in'].s,first['hot_in'].fluid)
+                        outputs.comp_W = (comp_out_h_ideal - first['comp_in'].h)/settings.comp_eff
+                        
+                        first['hot_in'].h = first['comp_in'].h + outputs.comp_W
+                        first['hot_in'].T = CP.PropsSI('T','H',first['hot_in'].h, 'P',first['hot_in'].p, first['hot_in'].fluid)
+                        
+                        first['expand_in'].h = first['hot_out'].h - settings.ihx_eff*ideal_dh
+                        first['expand_in'].T = CP.PropsSI('T','H',first['expand_in'].h,'P',first['expand_in'].p,first['expand_in'].fluid)
+                        first['expand_in'].s = CP.PropsSI('S','H',first['expand_in'].h,'P',first['expand_in'].p,first['expand_in'].fluid)
+                        expand_out_h_ideal = CP.PropsSI('H','P',first['cold_in'].p,'S',first['expand_in'].s,first['cold_in'].fluid)
+                        outputs.expand_W = (first['expand_in'].h - expand_out_h_ideal)*settings.expand_eff
+                        
+                        first['cold_in'].h = first['expand_in'].h - outputs.expand_W
+                        first['cold_in'].T = CP.PropsSI('T','H',first['cold_in'].h, 'P',first['cold_in'].p, first['cold_in'].fluid)
+                        
+                        if process_case <= 2:
+                            first['cold_in'].q = -second['cold_in'].q
+                            first['cold_in'].m = first['cold_in'].q / (first['cold_out'].h - first['cold_in'].h)
+                            first['cold_out'].m = first['cold_in'].m
+                            first['hot_in'].m = first['cold_in'].m
+                            first['hot_out'].m = first['cold_in'].m
+                            (dT_cold, T_rvs_cold) = self.HX_module(first['cold_in'], first['cold_out'], second['cold_in'], second['cold_out'], settings)
+                            
+                            if T_rvs_cold == 1:
+                                T_cout_ub = T_cout
+                            else:    
+                                T_cold_err = (settings.cold_T_pp - dT_cold)/settings.cold_T_pp
+                                if T_cold_err < 0:
+                                    T_cout_lb = T_cout
+                                else:
+                                    T_cout_ub = T_cout
+                                
+                                if abs(T_cold_err) < 1.0e-3:
+                                    c = 0
+                                else:
+                                    if T_cout_ub - T_cout_lb <= 0.01:
+                                        c = 0
+                        else:
+                            first['hot_in'].q = -second['hot_in'].q
+                            first['hot_in'].m = first['hot_in'].q / (first['hot_out'].h - first['hot_in'].h)
+                            first['hot_out'].m = first['hot_in'].m
+                            first['cold_in'].m = first['hot_in'].m
+                            first['cold_out'].m = first['hot_in'].m
+                            (dT_hot, T_rvs_hot) = self.HX_module(first['hot_in'], first['hot_out'], second['hot_in'], second['hot_out'], settings)
+                            
+                            if T_rvs_hot == 1:
+                                T_hout_lb = T_hout
+                            else:    
+                                T_hot_err = (settings.hot_T_pp - dT_hot)/settings.hot_T_pp
+                                if T_hot_err < 0:
+                                    T_hout_ub = T_hout
+                                else:
+                                    T_hout_lb = T_hout
+                                
+                                if abs(T_hot_err) < 1.0e-3:
+                                    c = 0
+                                else:
+                                    if T_hout_ub - T_hout_lb <= 0.01:
+                                        c = 0
+                    
+                    if process_case <= 2:
+                        first['hot_in'].q = first['hot_in'].m*(first['hot_out'].h - first['hot_in'].h)
+                        second['hot_in'].q = -first['hot_in'].q
+                        if process_case == 0:
+                            second['hot_in'].h = second['hot_out'].h - second['hot_in'].q/second['hot_out'].m
+                            second['hot_in'].T = CP.PropsSI('T','H',second['hot_in'].h,'P',second['hot_in'].p,second['hot_in'].fluid)
+                        elif process_case == 1:
+                            second['hot_out'].h = second['hot_in'].h + second['hot_in'].q/second['hot_in'].m
+                            second['hot_out'].T = CP.PropsSI('T','H',second['hot_out'].h,'P',second['hot_out'].p,second['hot_out'].fluid)
+                        elif process_case == 2:
+                            second['hot_in'].m = second['hot_in'].q/(second['hot_out'].h - second['hot_in'].h)
+                        
+                        (dT_hot, T_rvs_hot) = self.HX_module(first['hot_in'], first['hot_out'], second['hot_in'], second['hot_out'], settings)
+                        
+                        if T_rvs_hot == 1:
+                            T_hout_lb = T_hout
+                        else:    
+                            T_hot_err = (settings.hot_T_pp - dT_hot)/settings.hot_T_pp
+                            if T_hot_err < 0:
+                                T_hout_ub = T_hout
+                            else:
+                                T_hout_lb = T_hout
+                            
+                            if abs(T_hot_err) < 1.0e-3:
+                                b = 0
+                            else:
+                                if T_hout_ub - T_hout_lb <= 0.01:
+                                    b = 0
+                    else:
+                        first['cold_in'].q = first['cold_in'].m*(first['cold_out'].h - first['cold_in'].h)
+                        second['cold_in'].q = -first['cold_in'].q
+                        if process_case == 3:
+                            second['cold_in'].h = second['cold_out'].h + outputs.cold_Q/second['cold_in'].m
+                            second['cold_in'].T = CP.PropsSI('T','H',second['cold_in'].h,'P',second['cold_in'].p,second['cold_in'].fluid)
+                        elif process_case == 4:
+                            second['cold_out'].h = second['cold_in'].h - outputs.cold_Q/second['cold_out'].m
+                            second['cold_out'].T = CP.PropsSI('T','H',second['cold_out'].h,'P',second['cold_out'].p,second['cold_out'].fluid)
+                        elif process_case == 5:
+                            second['cold_in'].m = second['cold_in'].q/(second['cold_out'].h - second['cold_in'].h)
+                        
+                        first['cold_out'].m = first['cold_in'].m
+                        (dT_cold, T_rvs_cold) = self.HX_module(first['cold_in'], first['cold_out'], second['cold_in'], second['cold_out'], settings)
+                        
+                        if T_rvs_cold == 1:
+                            T_cout_ub = T_cout
+                        else:    
+                            T_cold_err = (settings.cold_T_pp - dT_cold)/settings.cold_T_pp
+                            if T_cold_err < 0:
+                                T_cout_lb = T_cout
+                            else:
+                                T_cout_ub = T_cout
+                            
+                            if abs(T_cold_err) < 1.0e-3:
+                                b = 0
+                            else:
+                                if T_cout_ub - T_cout_lb <= 0.01:
+                                    b = 0
                     
                 COP_cal = (first['hot_in'].h - first['hot_out'].h)/(outputs.comp_W/settings.mech_eff - outputs.expand_W*settings.gen_eff)
                 
@@ -233,37 +343,13 @@ class Brayton():
                     if (PR_ub - PR_lb)/(0.25*(PR_lb + PR_ub)*(2+beta)) < self.tol:
                         outputs.COP = results_array[-1][0]
                         a = 0
-        
-        if process_case <= 2:
-            first['hot_in'].m = first['cold_in'].m
-            first['hot_out'].m = first['cold_in'].m
-        else:
-            first['cold_in'].m = first['hot_in'].m
-            first['cold_out'].m = first['hot_in'].m
-        
+            
         outputs.comp_W = outputs.comp_W*first['cold_in'].m
         outputs.expand_W = outputs.expand_W*first['hot_in'].m
         outputs.ihx_Q = (first['hot_out'].h - first['expand_in'].h)*first['hot_in'].m
         outputs.hot_Q = (first['hot_in'].h - first['hot_out'].h)*first['hot_in'].m
         outputs.cold_Q = (first['cold_out'].h - first['cold_in'].h)*first['cold_in'].m
         
-        if process_case == 0:
-            second['hot_in'].h = second['hot_out'].h - outputs.hot_Q/second['hot_out'].m
-            second['hot_in'].T = CP.PropsSI('T','H',second['hot_in'].h,'P',second['hot_in'].p,second['hot_in'].fluid)
-        elif process_case == 1:
-            second['hot_out'].h = second['hot_in'].h + outputs.hot_Q/second['hot_in'].m
-            second['hot_out'].T = CP.PropsSI('T','H',second['hot_out'].h,'P',second['hot_out'].p,second['hot_out'].fluid)
-        elif process_case == 2:
-            second['hot_in'].m = outputs.hot_Q/(second['hot_out'].h - second['hot_in'].h)
-        elif process_case == 3:
-            second['cold_in'].h = second['cold_out'].h + outputs.cold_Q/second['cold_in'].m
-            second['cold_in'].T = CP.PropsSI('T','H',second['cold_in'].h,'P',second['cold_in'].p,second['cold_in'].fluid)
-        elif process_case == 4:
-            second['cold_out'].h = second['cold_in'].h - outputs.cold_Q/second['cold_out'].m
-            second['cold_out'].T = CP.PropsSI('T','H',second['cold_out'].h,'P',second['cold_out'].p,second['cold_out'].fluid)
-        elif process_case == 5:
-            second['cold_in'].m = outputs.cold_Q/(second['cold_in'].h - second['cold_out'].h)
-            
         hot_h_out_ideal = CP.PropsSI('H','T',second['hot_in'].T,'P',first['hot_out'].p,first['hot_out'].fluid)
         outputs.hot_eff = outputs.hot_Q/(first['hot_in'].h - hot_h_out_ideal)/first['hot_in'].m
         
@@ -271,6 +357,52 @@ class Brayton():
         outputs.cold_eff = outputs.cold_Q/(cold_h_out_ideal - first['cold_in'].h )/first['cold_in'].m
         
         return (first, second, outputs)
+    
+    def HX_module(self, first_in, first_out, second_in, second_out, inputs):
+        h_primary = np.zeros(shape=(inputs.N_element+1))
+        T_primary = np.zeros(shape=(inputs.N_element+1))
+        P_primary = np.zeros(shape=(inputs.N_element+1))
+        T_secondary = np.zeros(shape=(inputs.N_element+1))
+        h_secondary = np.zeros(shape=(inputs.N_element+1))
+        P_secondary = np.zeros(shape=(inputs.N_element+1))
+        dT = np.zeros(shape=(inputs.N_element+1))
+        
+        P_primary[0]= first_in.p
+        T_primary[0]= first_in.T
+        h_primary[0]= first_in.h
+        h_secondary[0]= second_out.h
+        T_secondary[0]= second_out.T
+        P_secondary[0]= second_out.p
+        
+        dT[0] = T_primary[0] - T_secondary[0]
+        
+        if (dT[0] < 0.0 and first_in.q < 0.0) or (dT[0] > 0.0 and first_in.q > 0.0):
+            T_rvs = 1
+        else:
+            T_rvs = 0
+            
+        for i in range(inputs.N_element):
+            if T_rvs == 1:
+                break
+            
+            h_primary[i+1] = h_primary[i] + first_in.q/inputs.N_element/first_in.m
+            P_primary[i+1] = P_primary[i] - (first_in.p - first_out.p)/inputs.N_element
+            T_primary[i+1] = CP.PropsSI("T","H",h_primary[i+1],"P",P_primary[i+1],first_in.fluid)
+            
+            h_secondary[i+1] = h_secondary[i] + first_in.q/inputs.N_element/second_in.m
+            P_secondary[i+1] = P_secondary[i] + (second_in.p - second_out.p)/inputs.N_element
+            T_secondary[i+1] = CP.PropsSI("T","H",h_secondary[i+1],"P",P_secondary[i+1],second_in.fluid)
+            
+            dT[i+1] = T_primary[i+1] - T_secondary[i+1]
+            
+            if (dT[i+1] < 0.0 and first_in.q < 0.0) or (dT[i+1] > 0.0 and first_in.q > 0.0):
+                T_rvs = 1
+            else:
+                T_rvs = 0
+                
+        T_pp = min(abs(dT))
+        
+        return(T_pp, T_rvs)
     
     def Post_process(self, first, second, settings, outputs):
         print("COP: %.3f" %(outputs.COP))
@@ -288,22 +420,22 @@ class Brayton():
     
 if __name__ == "__main__":
     
-    max_p = 4.0e5
+    max_p = 5.0e5
     
     hot_fluid = 'air'
-    cold_fluid = 'air'
+    cold_fluid = 'Water'
     ref_fluid = 'air'
     
-    hot_in_T = 500+273.15
-    hot_in_p = 101300.0
-    hot_out_T = 510+273.15
-    hot_out_p = 101300.0
+    hot_in_T = 350+273.15
+    hot_in_p = 18.0e6
+    hot_out_T = 400+273.15
+    hot_out_p = 18.0e6
     hot_m = 1.0
     
-    cold_in_T = 300+273.15
-    cold_out_T = 290+273.15
-    cold_in_p = 101300.0
-    cold_out_p = 101300.0
+    cold_in_T = 270+273.15
+    cold_out_T = 260+273.15
+    cold_in_p = 5.0e6
+    cold_out_p = 5.0e6
     cold_m = 0.0
     
     hot_in_sec = Fluid_stream(T=hot_in_T, p=hot_in_p, m=hot_m, fluid=hot_fluid)
@@ -320,7 +452,7 @@ if __name__ == "__main__":
     first = {'hot_in':hot_in_pri, 'hot_out':hot_out_pri, 'cold_in': cold_in_pri, 'cold_out':cold_out_pri, 'comp_in':comp_in_pri, 'expand_in':expand_in_pri}
     second = {'hot_in':hot_in_sec, 'hot_out':hot_out_sec, 'cold_in':cold_in_sec, 'cold_out':cold_out_sec}
     
-    settings = Settings(hot_dp = 0.001, hot_T_pp=5, cold_dp = 0.0, cold_T_pp = 5.0, ihx_cold_dp = 0.0, ihx_hot_dp = 0.0, ihx_eff = 0.9, comp_eff = 0.75, expand_eff = 0.85, gen_eff=0.99)
+    settings = Settings(hot_dp = 0.01, hot_T_pp=5, cold_dp = 0.01, cold_T_pp = 5.0, ihx_cold_dp = 0.01, ihx_hot_dp = 0.01, ihx_eff = 0.9, comp_eff = 0.75, expand_eff = 0.85, gen_eff=0.99, N_element=30)
     outputs = Outputs()
     
     airHP = Brayton()
